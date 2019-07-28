f53235fb53b104cc96fef81c9c9b79ce650afb26
slikts <dabas@untu.ms>
2019-07-28 19:15:57 +0300

    Update

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import json from 'rollup-plugin-json'
+
+const libraryName = 'Queueable'
+
+export default [
+	{
+		input: 'src/main.js',
+		output: {
+			name: 'howLongUntilLunch',
+			file: pkg.browser,
+			format: 'umd'
+		},
+		plugins: [
+			resolve(), // so Rollup can find `ms`
+			commonjs() // so Rollup can convert `ms` to an ES module
+		]
+	},
+
+	{
+		input: 'src/main.js',
+		external: ['ms'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
